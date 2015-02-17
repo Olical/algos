99af67f48bf27bc4c589de8f5b837397afb71512
Oliver Caldwell <olliec87@gmail.com>
2015-02-17 21:05:35 +0000

    Added board and solver skeleton from docs.

--- 8puzzle/src/Board.java
+++ 8puzzle/src/Board.java
@@ -1,4 +1,34 @@
-
 public class Board {
+    public Board(int[][] blocks) {
+    } // construct a board from an N-by-N array of blocks
 
+    // (where blocks[i][j] = block in row i, column j)
+    public int dimension() {
+    } // board dimension N
+
+    public int hamming() {
+    } // number of blocks out of place
+
+    public int manhattan() {
+    } // sum of Manhattan distances between blocks and goal
+
+    public boolean isGoal() {
+    } // is this board the goal board?
+
+    public Board twin() {
+    } // a boadr that is obtained by exchanging two adjacent blocks in the same
+      // row
+
+    public boolean equals(Object y) {
+    } // does this board equal y?
+
+    public Iterable<Board> neighbors() {
+    } // all neighboring boards
+
+    public String toString() {
+    } // string representation of this board (in the output format specified
+      // below)
+
+    public static void main(String[] args) {
+    }
 }
