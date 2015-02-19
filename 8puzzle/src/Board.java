41c225e64f208ce8c7bf7f76e96a8fb1a00697de
Oliver Caldwell <olliec87@gmail.com>
2015-02-19 23:19:33 +0000

    Stubbed methods and some board work.

--- 8puzzle/src/Board.java
+++ 8puzzle/src/Board.java
@@ -1,36 +1,84 @@
+import java.util.Arrays;
+
 public class Board {
-    public Board(int[][] blocks) {
-    } // construct a board from an N-by-N array of blocks
+    private int n;
+    private char[] board;
 
+    // construct a board from an N-by-N array of blocks
     // (where blocks[i][j] = block in row i, column j)
+    public Board(int[][] blocks) {
+        n = blocks.length;
+        board = new char[n];
+
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < n; y++) {
+                board[nth(x, y)] = (char) blocks[x][y];
+            }
+        }
+    }
+
+    private int nth(int x, int y) {
+        return y + n * x;
+    }
+
+    // board dimension N
     public int dimension() {
-    } // board dimension N
+        return n;
+    }
 
+    // number of blocks out of place
     public int hamming() {
-    } // number of blocks out of place
+        return 0; // todo
+    }
 
+    // sum of Manhattan distances between blocks and goal
     public int manhattan() {
-    } // sum of Manhattan distances between blocks and goal
+        return 0; // todo
+    }
 
+    // is this board the goal board?
     public boolean isGoal() {
-    } // is this board the goal board?
+        boolean sorted = true;
 
+        for (int i = 0; i < board.length - 1; i++) {
+            if (board[i] > board[i + 1]) {
+                sorted = false;
+                break;
+            }
+        }
+
+        return sorted;
+    }
+
+    // a twin is a board that is obtained by exchanging two adjacent blocks in
+    // the same row
     public Board twin() {
-    } // a boadr that is obtained by exchanging two adjacent blocks in the same
-      // row
+        return this; // todo
+    }
 
-    public boolean equals(Object y) {
-    } // does this board equal y?
+    // does this board equal y?
+    public boolean equals(Object x) {
+        if (x == this)
+            return true;
+        if (x == null)
+            return false;
+        if (x.getClass() != this.getClass())
+            return false;
+        Board that = (Board) x;
+        return Arrays.equals(this.board, that.board);
+    }
 
+    // all neighboring boards
     public Iterable<Board> neighbors() {
-    } // all neighboring boards
+        return null; // todo
+    }
 
     public String toString() {
         StringBuilder s = new StringBuilder();
-        s.append(N + "\n");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                s.append(String.format("%2d ", tiles[i][j]));
+        s.append(n + "\n");
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < n; y++) {
+                s.append(String.format("%2d ", board[nth(x, y)]));
             }
             s.append("\n");
         }
--- 8puzzle/src/Solver.java
+++ 8puzzle/src/Solver.java
@@ -6,12 +6,15 @@ public class Solver {
     } // find a solution to the initial board (using the A* algorithm)
 
     public boolean isSolvable() {
+        return true; // todo
     } // is the initial board solvable?
 
     public int moves() {
+        return 0; // todo
     } // min number of moves to solve initial board; -1 if unsolvable
 
     public Iterable<Board> solution() {
+        return null; // todo
     } // sequence of boards in a shortest solution; null if unsolvable
 
     public static void main(String[] args) {
