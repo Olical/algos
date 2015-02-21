e0c3cfcd38ba2be14b53b2169db2569655b0a33b
Oliver Caldwell <olliec87@gmail.com>
2015-02-21 16:16:07 +0000

    Added rest of the board methods. Not sure if correct yet.

--- 8puzzle/src/Board.java
+++ 8puzzle/src/Board.java
@@ -8,13 +8,18 @@ public class Board {
     // (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
         n = blocks.length;
-        board = new char[n];
+        board = new char[n * n];
 
         for (int x = 0; x < n; x++) {
             for (int y = 0; y < n; y++) {
                 board[nth(x, y)] = (char) blocks[x][y];
             }
         }
+    }
+
+    private Board(int size, char[] blocks) {
+        n = size;
+        board = blocks;
     }
 
     private int nth(int x, int y) {
@@ -28,12 +33,28 @@ public class Board {
 
     // number of blocks out of place
     public int hamming() {
-        return 0; // todo
+        int h = 0;
+
+        for (int i = 0; i < n * n; i++) {
+            if (i != board[i]) {
+                h++;
+            }
+        }
+
+        return h;
     }
 
     // sum of Manhattan distances between blocks and goal
     public int manhattan() {
-        return 0; // todo
+        int m = 0;
+
+        for (int i = 0; i < n * n; i++) {
+            if (i != board[i]) {
+                m += Math.abs(i - board[i]);
+            }
+        }
+
+        return m;
     }
 
     // is this board the goal board?
@@ -53,7 +74,18 @@ public class Board {
     // a twin is a board that is obtained by exchanging two adjacent blocks in
     // the same row
     public Board twin() {
-        return this; // todo
+        // If center top is blank, use second row.
+        // Otherwise if the left top is not 0, use the two top left.
+        // If that's zero then the two top right are correct.
+        // Presuming a 3x3 grid, but the rules will apply to all other sizes
+        // greater than it.
+        if (board[1] == 0) {
+            return swap(0, 1, 1, 1);
+        } else if (board[0] != 0) {
+            return swap(0, 0, 1, 0);
+        } else {
+            return swap(1, 0, 2, 0);
+        }
     }
 
     // does this board equal y?
@@ -70,7 +102,46 @@ public class Board {
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        return null; // todo
+        Stack<Board> neighbors = new Stack<Board>();
+        int x = 0;
+        int y = 0;
+
+        find: for (; x < n; x++) {
+            for (; y < n; y++) {
+                if (board[nth(x, y)] == 0) {
+                    break find;
+                }
+            }
+        }
+
+        if (x != 0) {
+            neighbors.push(swap(x, y, x - 1, y));
+        }
+
+        if (x != n - 1) {
+            neighbors.push(swap(x, y, x + 1, y));
+        }
+
+        if (y != 0) {
+            neighbors.push(swap(x, y, x, y - 1));
+        }
+
+        if (y != n - 1) {
+            neighbors.push(swap(x, y, x, y + 1));
+        }
+
+        return neighbors;
+    }
+
+    private Board swap(int x1, int y1, int x2, int y2) {
+        char[] swapped = board.clone();
+        int l = nth(x1, y1);
+        int r = nth(x2, y2);
+
+        swapped[l] = board[r];
+        swapped[r] = board[l];
+
+        return new Board(n, swapped);
     }
 
     public String toString() {
@@ -78,7 +149,7 @@ public class Board {
         s.append(n + "\n");
         for (int x = 0; x < n; x++) {
             for (int y = 0; y < n; y++) {
-                s.append(String.format("%2d ", board[nth(x, y)]));
+                s.append(String.format("%2d ", (int) board[nth(x, y)]));
             }
             s.append("\n");
         }
@@ -86,5 +157,12 @@ public class Board {
     }
 
     public static void main(String[] args) {
+        int[][] m = { { 3, 1, 0 }, { 4, 2, 5 }, { 7, 8, 6 } };
+        Board b = new Board(m);
+        StdOut.println(b);
+
+        for (Board bi : b.neighbors()) {
+            StdOut.println(bi);
+        }
     }
 }
